74b077fcaf8b0e36b5783e3640af80935fdc34c4
omerfarukz <ulrozremo@gmail.com>
2015-01-12 15:17:19 +0200

    lib v0

---
+++ OMR.Deasciifier/DeasciifyHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace OMR.Deasciifier
+{
+    public static class DeasciifyHelper
+    {
+        public static string Deasciify(this string asciiString)
+        {
+            var da = new Deasciifier(asciiString);
+            return da.ConvertToTurkish();
+        }
+    }
+}
